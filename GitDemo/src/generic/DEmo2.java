b6d2ef3c67e15dfda4a05d959c3c33d9a602b329
nsneeraj <46974049+nsneeraj@users.noreply.github.com>
2019-02-26 00:15:46 +0530

    hii

---
+++ GitDemo/src/generic/DEmo2.java
@@ -0,0 +1,14 @@
+package generic;
+
+public class DEmo2 {
+
+	public DEmo2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
