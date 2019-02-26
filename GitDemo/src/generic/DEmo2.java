40b1faeab3ae866c9175495cd4f3182a5659354b
nsneeraj <46974049+nsneeraj@users.noreply.github.com>
2019-02-26 21:58:48 +0530

    jljlj

--- GitDemo/src/generic/DEmo2.java
+++ GitDemo/src/generic/DEmo2.java
@@ -1,14 +1,34 @@
 package generic;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class DEmo2 {
 
-	public DEmo2() {
-		// TODO Auto-generated constructor stub
-	}
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String s = "Neeraj";
+System.out.println();
+	}
+	public static String removeDup(String str) {
+		
+		Set S = new HashSet();
+		StringBuffer sf = new StringBuffer();
+		
+		for(int i =0;i<str.length();i++)
+		{
+			char c = str.charAt(i);
+			if(!S.add(c)) {
+				S.add(c);
+				sf.append(c);
+				
+			}
+		}
+		
+		
+		
+		
+		return sf.toString();
+		
 	}
 
 }
---
+++ GitDemo/src/generic/Demo4.java
@@ -0,0 +1,12 @@
+package generic;
+
+public class Demo4 extends DEmo2 {
+
+	public static void main(String[] args) {
+		
+	}
+
+	
+	}
+
+
